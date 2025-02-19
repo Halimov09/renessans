f3169bdbf0a206dd99a3c99526d9c56add2ccc20
halimov09 <sunnatullohhalimov@gmail.com>
2025-02-19 16:50:12 +0500

    remove darks

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         
 
 <footer class="rounded-lg darks shadow-sm dark:bg-gray-900 ">
-    <div class="w-full max-w-screen-xl darks mx-auto p-4 md:py-8">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                 <img src={Logo2} class="h-8" alt="Flowbite Logo" />
