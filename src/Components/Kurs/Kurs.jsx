76fdff7bfa939987e53f4858a1dae7b84f0fc748
halimov09 <sunnatullohhalimov@gmail.com>
2025-03-13 23:39:53 +0500

    CODE-RUB: Remove none Users

--- src/Components/Kurs/Kurs.jsx
+++ src/Components/Kurs/Kurs.jsx
@@ -35,36 +35,6 @@ const posts = [
           href: '#',
           imageUrl: imgDesign},
       },
-      {
-        id: 1,
-        title: 'Boost your conversion rate',
-        href: '#',
-        description:
-          'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-        date: 'Mar 16, 2020',
-        datetime: '2020-03-16',
-        category: { title: 'Marketing', href: '#' },
-        author: {
-          name: 'Michael Foster',
-          role: 'Co-Founder / CTO',
-          href: '#',
-          imageUrl: imgfound},
-      },
-      {
-        id: 1,
-        title: 'Boost your conversion rate',
-        href: '#',
-        description:
-          'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-        date: 'Mar 16, 2020',
-        datetime: '2020-03-16',
-        category: { title: 'Marketing', href: '#' },
-        author: {
-          name: 'Michael Foster',
-          role: 'Co-Founder / CTO',
-          href: '#',
-          imageUrl: imgpriv},
-      },
   ]
   
 const Kurs = () => {
